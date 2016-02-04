928a655ae9a0817bdd4dff3dd9d3439c3b11843b
VincentPS <vincentschaddelee@hotmail.com>
2016-02-04 11:49:15 +0100

    blij eerste commit
    helemaal mooi

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+	<head>
+		<title>Chat</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<meta charset="UTF-8">
+	</head>
+	
+	<body>
+	
+		<form action="send.php" method="post" class="verzender">
+			<p>Message:</p>
+		  <input type="text" name="message">
+		  <input type="submit" name="enter" id="enter" value="Enter" class="send">
+		</form>
+		
+	</body>
